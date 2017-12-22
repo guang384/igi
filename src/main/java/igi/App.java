4aebd6ef410a53155e8c2d07ecd68dd56221f507
cuizg <cuizg.bri@chinatelecom.cn>
2017-12-22 15:09:32 +0800

    init

---
+++ src/main/java/igi/App.java
@@ -0,0 +1,13 @@
+package igi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/igi/UnitTest.java
@@ -0,0 +1,13 @@
+package igi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest {
+
+    @Test
+    public void test() {
+        System.out.println(11);
+        Assert.assertEquals(1, 1);
+    }
+}
